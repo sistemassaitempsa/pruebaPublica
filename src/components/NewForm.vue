a5f37bf7a0809028922ba0e6cd01e0d7f7773221
sistemassaitempsa <sistemassaitempsa@saitempsa.com>
2024-09-26 10:47:50 -0500

    se importa el componente search list y se buscan los paises

--- src/components/NewForm.vue
+++ src/components/NewForm.vue
@@ -1,80 +1,453 @@
 <template>
-  <div class="form-container">
-    <h2>Registro de datos</h2>
-    <form @submit.prevent="submitForm">
-      <div class="form-group">
-        <label for="nombre">Nombre</label>
-        <input
+  <div class="container">
+  <h2>Registro de datos</h2>
+  <div id="seccion">
+  <form @submit.prevent="submitForm" class="row g-3">
+    <h5>1. Información personal</h5>
+    <div class="info_container">
+    <div class="row">
+      <div class="col">
+        <label class="form-label" >Primer Nombre:*</label>
+        <input class="form-control"
           type="text"
-          v-model="form.nombre_contacto"
+          v-model="form.nom_emp"
           id="nombre"
           required
         />
       </div>
-
-      <div class="form-group">
-        <label for="apellido">Id</label>
-        <input
+      <div class="col">
+        <label class ="form-label" for="apellido">Segundo Nombre:</label class ="form-label">
+        <input class="form-control"
           type="text"
-          v-model="form.solicitante_id"
+          v-model="form.nom2_emp"
           id="apellido"
           required
         />
       </div>
-
-      <div class="form-group">
-        <label for="telefono">Teléfono</label>
-        <input type="tel" v-model="form.telefono" id="telefono" required />
+    </div>
+    <div class="row">
+      <div class="col">
+        <label class ="form-label" for="apellido">Primer Apellido:*</label class ="form-label">
+        <input class="form-control"
+          type="text"
+          v-model="form.ap1_emp"
+          id="apellido"
+          required
+        />
       </div>
-
-      <div class="form-group">
-        <label for="documento">Número de documento</label>
-        <input type="text" v-model="form.documento" id="documento" required />
+      <div class="col">
+        <label class ="form-label" for="apellido">Segundo Apellido:*</label class ="form-label">
+        <input class="form-control"
+          type="text"
+          v-model="form.ap2_emp"
+          id="apellido"
+          required
+        />
       </div>
+    </div>
+    <div class="row">
+      <div class="col">
+      <label class ="form-label" for="telefono">Tipo de documento:*</label class ="form-label">
+      <input class="form-control" type="tel" v-model="form.tip_ide" id="telefono" required />
+    </div>
 
-      <button type="submit">Enviar</button>
-    </form>
+    <div class="col">
+      <label class ="form-label" for="documento">Número de documento</label class ="form-label">
+      <input class="form-control" type="number" v-model="form.cod_emp" id="documento" required />
+    </div>
+    </div>
+   <div class="row">
+    <div class="col">
+        <SearchList
+              nombreCampo="Pais de expedicion: *"
+              @getPaises="getPaises"
+              eventoCampo="getPaises"
+              nombreItem="nom_pai"
+              :consulta="form.pai_exp"
+              :registros="paises"
+              :ordenCampo="1"
+              @getDepartamentos="getDepartamentos"
+              placeholder="Seleccione una opción"
+            />
+    </div>
+    <div class="col">
+      <label class ="form-label" for="documento">Departamento de expedicion</label class ="form-label">
+      <input class="form-control"
+        type="text"
+        v-model="form.dpt_exp"
+        id="documento"
+        required
+      />
+    </div>
+   </div>
+  <div class="row">
+    <div class="col">
+      <label class ="form-label" for="documento">Ciudad de expedicion</label class ="form-label">
+      <input class  ="form-control"
+        type="text"
+        v-model="form.ciu_exp"
+        id="documento"
+        required
+      />
+    </div>
+    <div class="col">
+        <SearchList
+              nombreCampo="Pais de nacimiento: *"
+              @getPaises="getPaises"
+              eventoCampo="getPaises"
+              nombreItem="nom_pai"
+              :consulta="form.cod_pai"
+              :registros="paises"
+              :ordenCampo="1"
+              @getDepartamentos="getDepartamentos"
+              placeholder="Seleccione una opción"
+            />
+    </div>
   </div>
+    <div class="row">
+      <div class="col">
+      <label class ="form-label" for="documento">Departamento de nacimiento</label class ="form-label">
+      <input class="form-control"
+        type="text"
+        v-model="form.cod_dep"
+        id="documento"
+        required
+      />
+    </div>
+    <div class="col">
+      <label class ="form-label" for="documento">Ciudad de nacimiento</label class ="form-label">
+      <input class="form-control"
+        type="text"
+        v-model="form.cod_ciu"
+        id="documento"
+        required
+      />
+    </div>
+    </div>
+   <div class ="row">
+    <div class="col">
+      <label class ="form-label" for="documento">Fecha de nacimiento</label class ="form-label">
+      <input class="form-control"
+        type="date"
+        v-model="form.fec_nac"
+        id="documento"
+        required
+      />
+    </div>
+    <div class="col">
+      <label class ="form-label" for="documento">Direccion residencia</label class ="form-label">
+      <input class="form-control"
+        type="text"
+        v-model="form.dir_res"
+        id="documento"
+        required
+      />
+    </div>
+   </div>
+    <div class="row">
+      <div class="col">
+      <label class ="form-label" for="documento">Fecha expedicion</label class ="form-label">
+      <input class="form-control"
+        type="date"
+        v-model="form.fec_expdoc"
+        id="documento"
+        required
+      />
+    </div>
+    <div class="col">
+      <label class ="form-label" for="documento">Telefono fijo</label class ="form-label">
+      <input class="form-control" type="text" v-model="form.tel_res" id="documento" required />
+    </div>
+    </div>
+   <div class="row">
+    <div class="col">
+      <label class ="form-label" for="documento">Telefono celular</label class ="form-label">
+      <input class="form-control" type="text" v-model="form.tel_cel" id="documento" required />
+    </div>
+    <div class="col">
+      <label class ="form-label" for="documento">Correo electronico</label class ="form-label">
+      <input class="form-control" type="text" v-model="form.e_mail" id="documento" required />
+    </div>
+   </div>
+   <div class="row">
+    <div class="col">
+        <SearchList
+              nombreCampo="Pais de residencia: *"
+              @getPaises="getPaises"
+              eventoCampo="getPaises"
+              nombreItem="nom_pai"
+              :consulta="form.pai_res"
+              :registros="paises"
+              :ordenCampo="1"
+              @getDepartamentos="getDepartamentos"
+              placeholder="Seleccione una opción"
+            />
+    </div>
+    <div class="col">
+      <label class ="form-label" for="documento">Departamento de residencia</label class ="form-label">
+      <input class="form-control"
+        type="text"
+        v-model="form.dpt_res"
+        id="documento"
+        required
+      />
+    </div>
+   </div>
+    <div class="row">
+      <div class="col">
+      <label class ="form-label" for="documento">Ciudad de residencia</label class ="form-label">
+      <input class="form-control"
+        type="text"
+        v-model="form.ciu_res"
+        id="documento"
+        required
+      />
+    </div>
+    <div class="col">
+      <label class ="form-label" for="documento">Banco</label class ="form-label">
+      <input class="form-control" type="text" v-model="form.cod_ban" id="documento" required />
+    </div>
+    </div>
+   
+    <div class="row">
+      <div class="col">
+      <label class ="form-label" for="documento">Numero de cuenta</label class ="form-label">
+      <input class="form-control" type="text" v-model="form.cta_ban" id="documento" required />
+    </div>
+    <div class="col">
+      <label class ="form-label" for="documento">Barrio</label class ="form-label">
+      <input class="form-control" type="text" v-model="form.barrio" id="documento" required />
+    </div>
+    </div>
+    <div class="row">
+      <div class="col">
+      <label class ="form-label" for="documento">Nivel académico</label class ="form-label">
+      <input class="form-control"
+        type="text"
+        v-model="form.Niv_aca"
+        id="documento"
+        required
+      />
+    </div>
+    <div class="col">
+      <label class ="form-label" for="documento">Identidad de género</label class ="form-label">
+      <input class="form-control"
+        type="text"
+        v-model="form.sex_emp"
+        id="documento"
+        required
+      />
+    </div>
+    </div>
+    <div class="row">
+      <div class="col">
+      <label class ="form-label" for="documento">Personas a cargo</label class ="form-label">
+      <input class="form-control"
+        type="text"
+        v-model="form.per_car"
+        id="documento"
+        required
+      />
+    </div>
+    <div class="col">
+      <label class ="form-label" for="documento">Grupo sanguíneo</label class ="form-label">
+      <input class="form-control"
+        type="text"
+        v-model="form.gru_san"
+        id="documento"
+        required
+      />
+    </div>
+    </div>
+    <div class="row">
+      <div class="col">
+      <label class ="form-label" for="documento">Factor RH</label class ="form-label">
+      <input class="form-control" type="text" v-model="form.fac_rhh" id="documento" required />
+    </div>
+    <div class="col">
+      <label class ="form-label" for="documento">Grupo etnico</label class ="form-label">
+      <input class="form-control" type="text" v-model="form.raza" id="documento" required />
+    </div>
+    </div>
+    <div class="row">
+      <div class="col">
+      <label class ="form-label" for="documento">Estado civil</label class ="form-label">
+      <input class="form-control" type="text" v-model="form.est_civ" id="documento" required />
+    </div>
+    <div class="col">
+      <label class ="form-label" for="documento">Estrato socioeconomico</label class ="form-label">
+      <input class="form-control" type="text" v-model="form.est_soc" id="documento" required />
+    </div>
+    </div>
+  </div>
+    <!-- Referencias personales -->
+    <!-- referencia 1 -->
+    <h5>2. Referencias personales</h5>
+     <div class="info_container">
+<div class="row">
+  <div class="col">
+      <label class ="form-label" for="documento">Tipo de referencia</label class ="form-label">
+      <select name="" id="" v-model="form.referencia1" required class="form-select">
+        <option value="1">Personal</option>
+        <option value="2">Familiar</option>
+      </select>
+    </div>
+    <div class="col">
+      <label class ="form-label" for="documento">Parentesco</label class ="form-label">
+      <input class="form-control" type="text" v-model="form.parentesco1" id="documento" required />
+    </div>
+    <div class="col">
+      <label class ="form-label" for="documento">Nombre completo</label class ="form-label">
+      <input class="form-control"
+        type="text"
+        v-model="form.nomrbre_referencia1"
+        id="documento"
+        required
+      />
+    </div>
+    <div class="col">
+      <label class ="form-label" for="documento">Numero de cedula</label class ="form-label">
+      <input class="form-control"
+        type="text"
+        v-model="form.cedula_referencia1"
+        id="documento"
+        required
+      />
+    </div>
+</div>
+<div class="row">
+  <div class="col">
+      <label class ="form-label" for="documento">Tipo de referencia</label>
+      <select name="" id="" v-model="form.referencia2" required class="form-select">
+        <option value="1">Personal</option>
+        <option value="2">Familiar</option>
+      </select>
+    </div>
+    <div class="col">
+      <label class ="form-label" for="documento">Parentesco</label class ="form-label">
+      <input class="form-control" type="text" v-model="form.parentesco2" id="documento" required />
+    </div>
+    <div class="col">
+      <label class ="form-label" for="documento">Nombre completo</label class ="form-label">
+      <input class="form-control"
+        type="text"
+        v-model="form.nomrbre_referencia2"
+        id="documento"
+        required
+      />
+    </div>
+    <div class="col">
+      <label class ="form-label" for="documento">Numero de cedula</label class ="form-label">
+      <input class="form-control"
+        type="text"
+        v-model="form.cedula_referencia2"
+        id="documento"
+        required
+      />
+    </div>
+</div>
+</div>
+<h5>3. Hijos</h5>
+<div class="info_container">
+  <div class="row">
+  <div class="col">
+      <label class ="form-label" for="documento">Primer apellido</label class ="form-label">
+      <input class="form-control"
+        type="text"
+        v-model="form.hijos_apellido"
+        id="documento"
+        required
+      />
+    </div>
+    <div class="col">
+      <label class ="form-label" for="documento">Segundo apellido</label class ="form-label">
+      <input class="form-control"
+        type="text"
+        v-model="form.hijos_apellido2"
+        id="documento"
+        required
+      />
+    </div>
+    <div class="col">
+      <label class ="form-label" for="documento">Nombre</label class ="form-label">
+      <input class="form-control" type="text" v-model="form.hijos_nombre" id="documento" required />
+    </div>
+</div>
+</div>
+
+ 
+
+    <button type="submit">Enviar</button>
+  </form>
+</div>
+</div>
 </template>
 
 <script>
-import axios from "axios"; // Importa axios
+import axios from "axios"; 
+import SearchList from "./SearchList.vue";
 
 export default {
+  components: {
+    SearchList},
   data() {
     return {
-      URL_API:
-        "https://debidadiligencia.saitempsa.com:8484/aplicaciones/api/public/",
+      paises:"",
+      URL_API:"http://localhost:8080/aplicaciones/api/public/",
       form: {
-        nombre_contacto: "",
-        solicitante_id: "",
-        telefono: "",
-        correo: "",
+        nom_emp:"",
+        nom2_emp:"",
+        cod_grupo:"",
+        cod_emp:"",
+        ap1_emp:"",
+        ap2_emp:"",
+        tip_ide:"",
+        pai_exp:"",
+        dpt_exp:"",
+        ciu_exp:"",
+        cod_pai:"",
+        cod_dep:"",
+        cod_ciu:"",
+        fec_nac:"",
+        dir_res:"",
+        fec_expdoc:"",
+        tel_res:"",
+        tel_cel:"",
+        e_mail:"",
+        pai_res:"",
+        dpt_res:"",
+        ciu_res:"",
+        cod_ban:"",
+        cta_ban:"",
+        barrio:"",
+        Niv_aca:"",
+        sex_emp:"",
+        per_car:"",
+        gru_san:"",
+        fac_rhh:"",
+        raza:"",
+        est_civ:"",
+        est_soc:""
+
       },
     };
   },
   methods: {
-    urlExterna() {
-      const urlCompleta = window.location.href;
-      if (urlCompleta.includes("debidadiligencia.saitempsa.com")) {
-        this.URL_API =
-          "https://debidadiligencia.saitempsa.com:8484/aplicaciones/api/public/";
+    getPaises() {
+      if (this.paises == "") {
+        let self = this;
+        axios
+          .get(self.URL_API + "api/v1/paisesFormularioEmpleado")
+          .then(function (result) {
+            self.paises = result.data;
+          });
       }
     },
-    configHeader() {
-      let config = {
-        headers: {
-          Authorization: "Bearer " + localStorage.getItem("access_token"),
-        },
-      };
-      return config;
-    },
     submitForm() {
-      let config = this.configHeader();
-      const url = `${this.URL_API}api/v1/seguimientocrm`; // Corrige el uso de this
+      const url = `http://localhost:8080/aplicaciones/api/public/api/v1/recepcionEmpleado`; // Corrige el uso de this
       console.log(this.form);
       axios
-        .post(url, this.form, config) // Corrige el acceso al objeto form
+        .post(url, this.form) // Corrige el acceso al objeto form
         .then((response) => {
           console.log(response.data);
           alert("Formulario enviado con éxito");
@@ -89,33 +462,23 @@ export default {
 </script>
 
 <style scoped>
-.form-container {
-  max-width: 400px;
-  margin: 0 auto;
-  padding: 20px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-}
-
 h2 {
   text-align: center;
 }
-
+.col{
+  margin-top: 1em;
+}
 .form-group {
   margin-bottom: 15px;
 }
-
-label {
-  display: block;
-  margin-bottom: 5px;
+#seccion {
+  border: solid #d5dbdb 0.5px;
+  padding: 30px;
+  margin-bottom: 30px;
+  border-radius: 10px;
+  box-shadow: rgba(0, 0, 0, 0.25) 0px 14px 28px,
+    rgba(0, 0, 0, 0.22) 0px 10px 10px;
 }
-
-input {
-  width: 100%;
-  padding: 8px;
-  box-sizing: border-box;
-}
-
 button {
   width: 100%;
   padding: 10px;
@@ -128,5 +491,13 @@ button {
 
 button:hover {
   background-color: #45a049;
+}
+.info_container{
+  border: solid rgba(214, 214, 214, 0.558) 1px;
+  border-radius: 5px;
+  padding: 2em
+}
+h5{
+  color: #45a049;
 }
 </style>
---
+++ src/components/SearchList.vue
@@ -0,0 +1,294 @@
+<template>
+  <div id="contenedor-select" class="was-validated">
+    <div class="mb-3">
+      <label for="exampleFormControlInput1" class="form-label">{{
+        nombreCampo
+      }}</label>
+      <div class="input-group">
+        <span class="input-group-text" id="basic-addon3"
+          ><i class="bi bi-search"></i
+        ></span>
+        <input
+          type="text"
+          @focus="consultaEndPoint()"
+          @click="hover = !hover"
+          @keyup="hover = true"
+          autocomplete="off"
+          @input="filterResults(registro, registros, 'registros')"
+          class="form-control"
+          id="exampleInputEmail"
+          :placeholder="placeholder"
+          aria-describedby="emailHelp"
+          v-model="registro"
+          :disabled="disabled"
+          :required="valida_campo"
+        />
+        <span
+          class="input-group-text"
+          style="
+            cursor: pointer;
+            border-top-right-radius: 5px;
+            border-bottom-right-radius: 5px;
+          "
+          @click="(registro = ''), listaEnCadena(registro)"
+          id="basic-addon3"
+          ><i class="bi bi-x"></i
+        ></span>
+        <div class="invalid-feedback">
+          {{ mensaje_error }}
+        </div>
+      </div>
+    </div>
+    <div
+      v-if="hover && registros.length > 0"
+      id="select1"
+      @mouseleave="hover = false"
+    >
+      <div
+        id="lista1"
+        v-for="(item, index) in registrosFilter"
+        :key="index"
+        @click="
+          (registro = nombreItems(item)),
+            retornoValorCampo(item),
+            (hover = !hover),
+            filterResults('', registros, 'registros'),
+            listaEnCadena(item)
+        "
+      >
+        {{ nombreItems(item) ? nombreItems(item).replace("null", "") : "" }}
+        <!-- {{ item.nom_pai != null ? item.nom_pai: item.nom_dep != null? item.nom_dep: item.nom_ciu != null ? item.nom_ciu: '' }} -->
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    nombreCampo: {},
+    placeholder: {},
+    registros: [],
+    nombreItem: {},
+    eventoCampo: {},
+    ubicacion: {},
+    clearInput: {
+      type: Boolean,
+      default: false,
+    },
+    index: {
+      type: Number,
+      default: null,
+    },
+    ordenCampo: {
+      type: Number,
+      default: null,
+    },
+    archivos: {
+      type: Boolean,
+      default: false,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    valida_campo: {
+      type: Boolean,
+      default: true,
+    },
+    consulta: {
+      type: String,
+      default: "",
+    },
+  },
+  data() {
+    return {
+      hover: false,
+      registro: "",
+      registrosFilter: this.registros,
+      mensaje_error: "¡Este campo debe ser diligenciado!",
+    };
+  },
+
+  watch: {
+    consulta: function () {
+      this.registro = this.consulta;
+    },
+    registros: function () {
+      this.registrosFilter = this.registros;
+    },
+    clearInput: function () {
+      if (this.clearInput == true) {
+        this.registro = "";
+      }
+    },
+  },
+  created() {
+    this.registro = this.consulta;
+  },
+  methods: {
+    consultaEndPoint() {
+      if (this.eventoCampo != undefined) {
+        this.$emit(this.eventoCampo);
+      }
+    },
+    retornoValorCampo(item) {
+      if (
+        this.registro != "" &&
+        this.nombreCampo != "Pais" &&
+        this.nombreCampo != "Departamento" &&
+        this.nombreCampo != "Ciudad" &&
+        this.index == null
+      ) {
+        this.$emit(this.eventoCampo, item);
+      } else if (this.index != null) {
+        this.$emit(this.eventoCampo, item, this.index);
+      }
+    },
+    nombreItems(item) {
+      return item[this.nombreItem];
+    },
+    filterResults(value, array, nombrearray) {
+      var search = null;
+      if (this.archivos) {
+        let self = this;
+        search = array.forEach(function (element) {
+          if (self.nombreItems(element) != undefined) {
+            return element;
+          }
+        });
+      } else {
+        search = array.filter((item) =>
+          this.nombreItems(item).toLowerCase().match(value.toLowerCase())
+        );
+        if (nombrearray == "registros") {
+          this.registrosFilter = search;
+        }
+      }
+    },
+    listaEnCadena(item) {
+      if (item == "") {
+        this.$emit(this.eventoCampo, null);
+      }
+      switch (this.eventoCampo) {
+        case "getPaises":
+          this.$emit("getDepartamentos", item, this.ordenCampo, this.index);
+          break;
+        case "getDepartamentos":
+          this.$emit("getMunicipios", item, this.ordenCampo, this.index);
+          break;
+        case "setMunicipios":
+          this.$emit("setMunicipios", item, this.ordenCampo, this.index);
+          break;
+        case "getCodigosCiiu":
+          this.$emit("getActividadesCiiu", item);
+          break;
+        case "getSubCategoryReports":
+          this.$emit("getSubCategoryReports", item);
+          break;
+        case "getTipoIdentificacion":
+          this.$emit(
+            "setTipoIdentificacion",
+            item,
+            this.ordenCampo,
+            this.index
+          );
+          break;
+        case "getAfirmacionNegacion":
+          this.$emit(
+            "setAfirmacionNegacion",
+            item,
+            this.ordenCampo,
+            this.index
+          );
+          break;
+        case "getBancos":
+          this.$emit("setBanco", item, this.ordenCampo, this.index);
+          break;
+        case "getTipoCuentaBancos":
+          this.$emit("setTipoCuentaBancos", item, this.ordenCampo, this.index);
+          break;
+        case "getTipoOrigenMedios":
+          this.$emit("setTipoOrigenMedios", item, this.ordenCampo);
+          break;
+        case "getTipoArhivo":
+          this.$emit("setTipoArchivo", item, this.index);
+          break;
+        case "getSubsidio":
+          this.$emit("setSubsidio", item, this.ordenCampo, this.index);
+          break;
+        case "getVacante":
+          this.$emit("setVacante", item, this.ordenCampo, this.index);
+          break;
+      }
+    },
+  },
+};
+</script>
+<style scoped>
+label {
+  float: left;
+  margin: 20px 0px 5px 0px;
+}
+
+/* Select con filtro personalizado */
+#contenedor-select {
+  position: relative;
+}
+
+#select1 {
+  z-index: 200;
+}
+
+#select2 {
+  z-index: 100;
+}
+
+#select1,
+#select2 {
+  padding: 5px;
+  border: solid #d5dbdb 0.5px;
+  border-radius: 10px;
+  text-align: left;
+  max-height: 250px;
+  width: 97%;
+  margin: auto;
+  overflow-y: auto;
+  position: absolute;
+  top: 85px;
+  left: 35px;
+  background-color: white;
+  cursor: pointer;
+}
+
+#seccion {
+  border: solid #d5dbdb 0.5px;
+  padding: 30px;
+  border-radius: 10px;
+}
+
+.tituloseccion {
+  position: relative;
+  display: inline-block;
+  background-color: white;
+  padding: 5px;
+  top: 22px;
+}
+
+#select1 #lista1,
+#select2 #lista2 {
+  padding: 5px;
+  cursor: pointer;
+  border-radius: 5px;
+  color: black;
+}
+
+#select1 #lista1:hover,
+#select2 #lista2:hover {
+  background-color: #0d6efd;
+  color: white;
+}
+
+span {
+  height: 38px;
+}
+</style>
